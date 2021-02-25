6aa45849e3e38c8881cdbbb7a9734c53b1817b61
sorhg <daehee.kim465@gmail.com>
2021-02-25 11:20:20 +0100

    fixing a line for app to use /static/public

--- homework/hw01-express/index.js
+++ homework/hw01-express/index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use('/static', express.static(__dirname + 'public'))
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Server ready at http://localhost:${port}`))
